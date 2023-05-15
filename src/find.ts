3ae90d8bcc32299715b6ceb74446d6936cb6bf5b
Damian <day-me-an@users.noreply.github.com>
2023-05-15 13:48:31 +0100

    Support Kustomize (#1)
    * basic Kustomize scenario working
    
    * check line number
    
    * support newName changes too
    
    * polish
    
    * remove unnecessary keepSourceTokens
    
    * polish
    
    * more checking
    
    * support files containing multiple YAML documents
    
    * move Kustomize text fixtures into own dir
    
    * test newName
    
    * increase extensibility via "Finder" type
    
    * support just a name change
    
    * cache yaml parsing

--- src/find.ts
+++ src/find.ts
@@ -1,6 +1,9 @@
+import fs from "node:fs";
 import parseDiff from "parse-diff";
+import YAML, { LineCounter, YAMLMap } from "yaml";
 
 const YAML_COMMENT_REGEX = /^\s*#/;
+const KUSTOMIZE_REGEX = /^\s*newName|newTag:/;
 
 interface DockerRegistry {
   endpoint: string;
@@ -10,6 +13,19 @@ export interface DockerURIMatch {
   uri: string;
   file: string;
   line: number;
+}
+
+type Finder = (change: Change) => DockerURIMatch;
+
+interface Change {
+  content: string;
+  lineNumber: number;
+  file: string;
+}
+
+interface ParsedYamlFile {
+  documents: YAML.Document[];
+  lineCounter: YAML.LineCounter;
 }
 
 export function dockerImageURIFinder(registries: DockerRegistry[]) {
@@ -23,6 +39,98 @@ export function dockerImageURIFinder(registries: DockerRegistry[]) {
         "gi"
       )
   );
+
+  // A cache of parsed YAML files to avoid parsing the same file on every change!
+  const yamlFiles = new Map<string, ParsedYamlFile>();
+
+  function parseYamlFile(path: string): ParsedYamlFile {
+    if (!yamlFiles.has(path)) {
+      const contents = fs.readFileSync(path).toString();
+      const lineCounter = new LineCounter();
+      const documents = YAML.parseAllDocuments(contents, { lineCounter });
+      yamlFiles.set(path, { lineCounter, documents });
+    }
+    return yamlFiles.get(path);
+  }
+
+  function uriFromKustomize(change: Change): DockerURIMatch {
+    const match = change.content.match(KUSTOMIZE_REGEX);
+    if (!match) {
+      return null;
+    }
+
+    // Parse the YAML file into an AST so we can traverse it.
+    const { documents, lineCounter } = parseYamlFile(change.file);
+
+    for (const document of documents) {
+      // We only care about Kustomization manifests.
+      if (document.get("kind") !== "Kustomization") {
+        continue;
+      }
+
+      let result: DockerURIMatch;
+
+      // Find the newTag node so we can look up the image it belongs to.
+      YAML.visit(document, {
+        Scalar(_, node, path) {
+          // We only care about the node on the change's line number.
+          if (lineCounter.linePos(node.range[0]).line !== change.lineNumber) {
+            return;
+          }
+
+          // Find the image entry from the "images" list.
+          const image = path[path.length - 2];
+          if (!(image instanceof YAMLMap)) {
+            return YAML.visit.SKIP;
+          }
+
+          const imageName = (image.get("newName") ??
+            image.get("name")) as string;
+
+          // Skip any images that don't belong to the registries we care about.
+          if (
+            !registries.some((registry) =>
+              imageName.startsWith(registry.endpoint)
+            )
+          ) {
+            return YAML.visit.SKIP;
+          }
+
+          const tag = image.get("newTag") ?? image.get("tag");
+
+          result = {
+            uri: imageName + ":" + tag,
+            line: change.lineNumber,
+            file: change.file,
+          };
+
+          return YAML.visit.BREAK;
+        },
+      });
+
+      if (result) {
+        return result;
+      }
+    }
+
+    return null;
+  }
+
+  function uriFromRegex(change: Change): DockerURIMatch {
+    for (const regex of regexes) {
+      const matches = change.content.matchAll(regex);
+      for (const match of matches) {
+        return {
+          uri: match[0],
+          line: change.lineNumber,
+          file: change.file,
+        };
+      }
+    }
+    return null;
+  }
+
+  const finders: Finder[] = [uriFromKustomize, uriFromRegex];
 
   return function find(diff: string): DockerURIMatch[] {
     const files = parseDiff(diff);
@@ -43,14 +151,26 @@ export function dockerImageURIFinder(registries: DockerRegistry[]) {
             continue;
           }
 
-          for (const regex of regexes) {
-            const matches = line.matchAll(regex);
-            for (const match of matches) {
-              uris.push({
-                uri: match[0],
+          // Try to find a Docker URI via one of the finders.
+          for (const finder of finders) {
+            let uri: DockerURIMatch;
+            try {
+              uri = finder({
+                content: line,
+                lineNumber: change.ln,
                 file: file.to,
-                line: change.ln,
               });
+            } catch (error) {
+              console.error(
+                `Error trying to extract URI via ${finder.name}:`,
+                error
+              );
+              continue;
+            }
+
+            if (uri) {
+              uris.push(uri);
+              break;
             }
           }
         }
