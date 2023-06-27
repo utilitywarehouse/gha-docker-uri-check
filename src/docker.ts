0d8f5a3dc03d98d9724d4e2079a48c958d64d4d6
Damian <dwood@uw.co.uk>
2023-06-27 09:48:55 +0100

    replace Docker client with custom

--- src/docker.ts
+++ src/docker.ts
@@ -1,4 +1,4 @@
-import { createClientV2 } from "docker-registry-client";
+import fetch from "node-fetch";
 
 export interface DockerRegistry {
   endpoint: string;
@@ -18,35 +18,86 @@ export function dockerRegistryChecker(registries: DockerRegistry[]) {
       throw Error("Couldn't find a registry for Docker URI");
     }
 
-    const [repoName, tag] = uri.split(":");
+    const client = createDockerClient(registry);
+    const [image, tag] = uri.replace(registry.endpoint + "/", "").split(":");
 
-    // Unfortunately, the docker-registry-client library requires a full registry URL, so we can't cache this.
-    const client = createClientV2({
-      name: repoName,
-      username: registry.username,
-      password: registry.password,
-    });
-
-    try {
-      await new Promise((resolve, reject) => {
-        client.getManifest({ ref: tag }, (err, manifest) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(manifest);
-          }
-        });
-      });
-    } catch (error) {
-      if (error.body?.code === "NotFoundError") {
-        return "not_found";
-      }
-      throw error;
-    } finally {
-      // Need to close otherwise the program hangs and never exits.
-      client.close();
+    const exists = await client.tagExists(image, tag);
+    if (!exists) {
+      return "not_found";
     }
 
     return "ok";
+  };
+}
+
+function createDockerClient(registry: DockerRegistry) {
+  let token = "none";
+
+  async function fetchApi(path, config) {
+    function apiFetch() {
+      return fetch(`https://${registry.endpoint}/v2/` + path, {
+        headers: {
+          ...config.headers,
+          Authorization: "Bearer " + token,
+        },
+      });
+    }
+
+    async function fetchToken(
+      realm: string,
+      service: string,
+      scope: string
+    ): Promise<string> {
+      const result = await fetch(
+        realm +
+          `?service=${encodeURIComponent(service)}&scope=${encodeURIComponent(
+            scope
+          )}`,
+        {
+          headers: {
+            Authorization:
+              "Basic " +
+              Buffer.from(registry.username + ":" + registry.password).toString(
+                "base64"
+              ),
+          },
+        }
+      );
+      if (!result.ok) {
+        throw new Error(result.statusText);
+      }
+      const data = (await result.json()) as { token: string };
+      return data.token;
+    }
+
+    let result = await apiFetch();
+
+    // This header means the client has to fetch a token and retry the original request.
+    const wwwAuthenticate = result.headers.get("www-authenticate");
+    if (wwwAuthenticate) {
+      // Not super robust, but good enough.
+      const realm = wwwAuthenticate.match(/realm="(.+?)"/)[1];
+      const service = wwwAuthenticate.match(/service="(.+?)"/)[1];
+      const scope = wwwAuthenticate.match(/scope="(.+?)"/)[1];
+
+      // Repeat the original request with the new token.
+      token = await fetchToken(realm, service, scope);
+      return await apiFetch();
+    }
+
+    return result;
+  }
+
+  return {
+    async tagExists(image: string, tag: string) {
+      const result = await fetchApi(image + "/manifests/" + tag, {
+        method: "HEAD",
+        headers: {
+          // Required for OCI images, which some namespaces use.
+          Accept: "application/vnd.oci.image.index.v1+json",
+        },
+      });
+      return result.ok;
+    },
   };
 }
