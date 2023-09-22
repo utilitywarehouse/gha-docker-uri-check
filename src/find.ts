8dc65b1a325920c08fe5d3fabc3276e155dc231c
Damian <dwood@uw.co.uk>
2023-09-22 10:05:26 +0100

    support dots in image names

--- src/find.ts
+++ src/find.ts
@@ -35,7 +35,7 @@ export function dockerImageURIFinder(registries: DockerRegistry[]) {
       new RegExp(
         "\\b" +
           escapeRegExp(registry.endpoint) +
-          "/[\\w_-]+/[\\w_-]+:([\\w\\._-]+)\\b",
+          "/[\\w_-]+/[\\w\\._-]+:([\\w\\._-]+)\\b",
         "gi"
       )
   );
